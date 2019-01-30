b16bbfa958b1f031f113794f2210e44951a97439
Jaesung Na <na-0003@hotmail.com>
2019-01-30 10:48:16 +0900

    eslint setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
 
-class App extends Component{
-    render(){
+class App extends Component {
+    render() {
         return (
-            <View>
+            <View style={{ flex: 1, backgroundColor: '#fff' }}>
                 <Text>An App</Text>
             </View>
         );
     }
 }
+
 export default App;
